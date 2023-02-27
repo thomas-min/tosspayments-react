81c1bd4f26cef812031c6cf60e2bc95a10061c83
Thomas Min <gyeongmin.min@ssupport.co.kr>
2023-02-27 11:06:35 +0900

    update : wrap widget instance in obj

--- src/contexts/PaymentWidgetContext.ts
+++ src/contexts/PaymentWidgetContext.ts
@@ -1,6 +1,10 @@
 import type { PaymentWidgetInstance } from "@tosspayments/payment-widget-sdk";
 import { createContext } from "react";
 
+export type PaymentWidgetContextValue = {
+  paymentWidget?: PaymentWidgetInstance;
+};
+
 export const PaymentWidgetContext = createContext<
-  PaymentWidgetInstance | undefined
+  PaymentWidgetContextValue | undefined
 >(undefined);
